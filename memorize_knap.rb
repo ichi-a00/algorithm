9206d654720a2faa8c90708fa28ceba589cc4536
A.I <88603810+ichi-a00@users.noreply.github.com>
2022-01-19 20:17:17 +0900

    Update memorize_knap.rb

--- memorize_knap.rb
+++ memorize_knap.rb
@@ -2,61 +2,53 @@ input_line = gets
 n = input_line.split(" ")[0].to_i
 x = input_line.split(" ")[1].to_i
 
-#dp配列を作成、dp[0][n]を初期化
-dp = Array.new(n+1).map{Array.new(n+1,x+1)}
-dp2 = Array.new(n+1).map{[]}
+#dp配列　i個目までで合計金額jにできる？ True/False を作成、dp[0][j]を初期化
+dp = Array.new(n+1).map{Array.new(x+1,false)}
+dp[0][0] = true
+
+#何個買ってるか
+dp_count = Array.new(n+1) {Array.new(x+1,0)}
+
 okashi = []
 
 for i in 0...n
     input_line = gets
-    price = input_line.to_i
-    okashi[i] = price
-    dp[i][0] = 0
+    okashi[i] = input_line.to_i
 end
 
-#okashi.sort
-tmp = []
+max_n=0
+max_j=0
 
 #MAP
 for i in 1..n
-    dp2 = tmp
-    for j in 0..n
+    for j in 0..x
         #p "i, j = #{i}, #{j}"
-        #p okashi[i-1]["w"]
-        if (1 > j) then
-            #すくわない　というかすくえない
+        
+        if (okashi[i-1] > j) then
+            #かえない
             dp[i][j] = dp[i-1][j]
+            dp_count[i][j] = dp_count[i-1][j]
         else
-            #すくう
-            buy = dp[i-1][j-1]+okashi[i-1]
+            #i個目までで合計金額jにできる？ True/False
+            dp[i][j] = dp[i-1][j-okashi[i-1]] || dp[i-1][j]
             
-            #すくわない
-            not_buy = dp[i-1][j]
-            
-            #p "fish: #{fish}"
-            #すくったほうがよければ更新
-            dp[i][j] = [buy, not_buy].min
-            
-
-            if buy<not_buy then
-                tmp.push(i)
-                #p "#{i}, #{j}"
+            #買うとできるとき
+            if dp[i-1][j-okashi[i-1]] then
+                dp_count[i][j] = dp_count[i-1][j-okashi[i-1]]
+                dp_count[i][j] += 1
+                #p "i, j = #{i}, #{j}"
+                #p dp_count[i][j]
+            else
+                #買わなければできるとき か　そもそもできないとき
+                dp_count[i][j] = dp_count[i-1][j]
             end
+        end
+        #最大種類数とその時の合計金額を更新
+        if max_n <= dp_count[i][j] then
+            max_n = dp_count[i][j]
+            max_j = j
         end
     end
 end
 
-max_n = -1
-min_price = 0
-
-#最大種類数と最小値段がとりあえずこれでわかる
-dp.min.each do |dp|
-    if dp-x < 0 then
-        max_n += 1
-        min_price = dp
-    end
-end
-
-#p max_n
-
-p dp2
+p x-max_j
